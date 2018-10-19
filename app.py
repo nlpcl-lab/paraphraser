cbaa7d58950722039a15450235f91b7fc611cdda
bowbowbow <clsrn1581@gmail.com>
2018-10-19 16:21:11 +0900

    Added server

--- app.py
+++ app.py
@@ -7,31 +7,29 @@ sys.path.append(base_dir)
 
 app = Flask(__name__)
 
+dir_path = '/home/webmaster/Web_API/API/Paraphrase_Generator'
+args = [
+    '{}/Paraphrase/moses'.format(dir_path), '-f',
+    '{}/Working/mert-work/moses.ini'.format(dir_path),
+    '-n-best-list',
+    '2',
+]
+proc = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+
+@app.route('/paraphrase', methods=['POST'])
+def annotate():
+    data = request.get_json()
+    sentence = data['sentence'] + '\n'
+    proc.stdin.write(sentence.encode())
+    proc.stdin.flush()
+    result = proc.stdout.readline().decode()
+    result = result.replace('\n', '')
+    return json.dumps({
+        'result': result,
+    })
+
+
 if __name__ == '__main__':
     FLASK_DEBUG = os.getenv('FLASK_DEBUG', True)
     app.run(host='0.0.0.0', debug=FLASK_DEBUG, port=8081)
-
-    dir_path = '/home/webmaster/Web_API/API/Paraphrase_Generator'
-    args = [
-        '{}/moses'.format(dir_path), '-f',
-        '{}/Working/mert-work/moses.ini'.format(dir_path),
-        '-n-best-list',
-        'output.txt',
-        2,
-    ]
-
-    with Popen(args, stdin=PIPE, stdout=PIPE) as proc:
-        proc.stdin.write('what is your name ?')
-        proc.stdin.flush()
-
-        out, err = proc.communicate()
-        print('out :', out)
-
-        @app.route('/paraphrase', methods=['POST'])
-        def annotate():
-            data = request.get_json()
-            sentence = data['sentence']
-
-            return json.dumps({
-                'result': '',
-            })
